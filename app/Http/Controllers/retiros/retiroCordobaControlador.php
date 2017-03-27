326ef330965ecd79c487c5923e1667c93d7c48bb
ariellugo92 <ariellugo92@koderlite.com>
2017-03-27 10:35:37 -0600

    iniciando los metodos del retiro

--- app/Http/Controllers/retiros/retiroCordobaControlador.php
+++ app/Http/Controllers/retiros/retiroCordobaControlador.php
@@ -7,5 +7,14 @@ use cajeroIA\Http\Controllers\Controller;
 
 class retiroCordobaControlador extends Controller
 {
-    //
+    // metodo para realizar la distribucion del dinero
+    public static function initDistribucionDinero($cant)
+    {
+        # code...
+    }
+
+    // metodo para saber cuanto dinero hay en cordobas
+    public static function getCantidadDinero(){
+        
+    }
 }
